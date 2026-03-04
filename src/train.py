e3afc25891471ea29553628f13956682d046265a
Cesar Alberto Tapia Mar <cesaralberto.tapia@bbva.com>
2026-03-04 10:40:11 -0600

    refactor: move logic from train to main

--- src/train.py
+++ src/train.py
@@ -3,22 +3,8 @@ import logging
 from logging import getLogger
 import tomli as tomllib
 
-logging.basicConfig(level=logging.INFO, format="%(levelname)s - %(message)s")
 
 logger = getLogger(__name__)
-
-def load_config(config_path: str = "config.toml") -> dict:
-    """Loads configuration from a TOML file.
-
-    Args:
-        config_path (str): The file path to the TOML configuration file.
-
-    Returns:
-        dict: The configuration dictionary.
-    """
-    with open(config_path, "rb") as f:
-        config = tomllib.load(f)
-    return config
 
 
 def read_data(path: str) -> pd.DataFrame:
@@ -32,11 +18,3 @@ def read_data(path: str) -> pd.DataFrame:
     """
     logger.info(f"Reading data from {path}")
     return pd.read_csv(path)
-
-if __name__ == "__main__":
-    config = load_config()
-
-    data_path = config["paths"]["raw_local"]
-
-    df = read_data(data_path)
-    logger.info(f"Data preview:\n{df.head()}")
