5852acd9dde40f847bb62e15860d93a8f8d51b88
Cesar Alberto Tapia Mar <cesaralberto.tapia@bbva.com>
2026-03-04 01:37:53 -0600

    feature: local path now is read from toml file

--- src/train.py
+++ src/train.py
@@ -1,10 +1,25 @@
 import pandas as pd
 import logging
 from logging import getLogger
+import tomli as tomllib
 
 logging.basicConfig(level=logging.INFO, format="%(levelname)s - %(message)s")
 
 logger = getLogger(__name__)
+
+def load_config(config_path: str = "config.toml") -> dict:
+    """Loads configuration from a TOML file.
+
+    Args:
+        config_path (str): The file path to the TOML configuration file.
+
+    Returns:
+        dict: The configuration dictionary.
+    """
+    with open(config_path, "rb") as f:
+        config = tomllib.load(f)
+    return config
+
 
 def read_data(path: str) -> pd.DataFrame:
     """Reads data from a CSV file and returns a DataFrame.
@@ -19,5 +34,9 @@ def read_data(path: str) -> pd.DataFrame:
     return pd.read_csv(path)
 
 if __name__ == "__main__":
-    df = read_data("C:\\learn_ml_gcp\\ML_with_GCP\\data\\raw.csv")
+    config = load_config()
+
+    data_path = config["paths"]["raw_local"]
+
+    df = read_data(data_path)
     logger.info(f"Data preview:\n{df.head()}")
