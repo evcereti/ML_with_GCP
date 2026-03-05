f096e729b17d9d572acfda64d5b5a7c0a3fed5b1
Cesar Alberto Tapia Mar <cesaralberto.tapia@bbva.com>
2026-03-05 17:37:22 -0600

    feature: add logic to validate types from schema

--- src/ingest.py
+++ src/ingest.py
@@ -19,7 +19,7 @@ def read_data(path: str) -> pd.DataFrame:
     return pd.read_csv(path)
 
 
-def validate_schema(df: pd.DataFrame, required_columns: list) -> bool:
+def validate_schema(df: DataFrame, schema_columns: dict) -> bool:
     """Validates the schema of the DataFrame.
 
     Args:
@@ -29,8 +29,33 @@ def validate_schema(df: pd.DataFrame, required_columns: list) -> bool:
         bool: True if the schema is valid, False otherwise.
     """
 
-    missing_columns = set(required_columns) - set(df.columns)
+    missing_columns = set(schema_columns.keys()) - set(df.columns)
 
     if missing_columns:
+        logger.info(f"Missing Columns: {missing_columns}")
         return False
+    
+    for column, schema_type in schema_columns.items():
+        logger.info(f"Validating type {column} : {schema_type}")
+        if df[column].dtype != schema_type:
+            logger.info(f"Invalid type for column: Should be {column}:{schema_type} but is {column}:{df[column].dtype}")
+            return False
+    
     return True
+
+
+def drop_columns(df: DataFrame, required_columns: list) -> DataFrame:
+    """Determines which columns to drop from the DataFrame.
+
+    Args:
+        df (DataFrame): The DataFrame to analyze.
+        required_columns (list): The list of required columns to keep.
+
+    Returns:
+        DataFrame: The DataFrame with the specified columns dropped.
+    """
+    drop_cols = [col for col in df.columns if col not in required_columns]
+
+    logger.info(f"Dropping columns: {drop_cols}")
+    df = df.drop(columns=drop_cols)
+    return df
